d0ae755e7174e0d7fe449b723b2d5e74e5ea2d86
Ali <mmadi_6@hotmail.com>
2023-08-30 15:26:55 +0200

    nouveau

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,17 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Laravel 9.X</title>
+  <title> quizz </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css')}}">
   <script src="{{ asset('bootstrap.bundle.min.js') }}"></script>
-  
+
 </head>
+
 <body>
-   
-    <div class="container">
-       @yield('content')
+  <div class="container">
+    <div class="alert alert-success" role="alert">
+      <h4 class="alert-heading"> conception!</h4>
+      <p class="lead"> Apllication de laravel </p>
+      <hr>
+      <p class="mb-0">developpement.</p>
+      @yield('content')
     </div>
 
 </body>
--- resources/views/quizz/index.blade.php
+++ resources/views/quizz/index.blade.php
@@ -3,4 +3,13 @@
 <div class="alert alert-info">
     phase de conception du projet quizz
 </div>
+<form>
+<div class="alert alert-info" role="alert">
+  <h4 class="alert-heading"> conception!</h4>
+  <p class="lead"> Apllication de laravel </p>
+  <hr>
+  <p class="mb-0">developpement.</p>
+</div>
+  
+</form>
  @endsection
