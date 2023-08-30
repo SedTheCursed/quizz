462ad041144b7eda52b330a7334b4b1f553a3c35
Lucy DO NASCIMENTO <lucynasci@gmail.com>
2023-08-30 15:10:33 +0200

    actualisation du form "foreach"

--- resources/views/quizz/index.blade.php
+++ resources/views/quizz/index.blade.php
@@ -4,20 +4,23 @@
     <label for="pseudo">Pseudonyme:</label><br>
     <input type="text" id="pseudo" name="pseudo"><br>
     <select name="sujet" id="sujet">
-        <option value="">Sélectionner le sujet:</option>
+        <option value="" disabled>Sélectionner le sujet:</option>
+        @foreach ($subjects as $subject)
+        <option value="{{$subject['id']}}">{{$subject['name']}}</option>
+        @endforeach
     </select>
     <fieldset>
         <legend>Sélectionner la difficulté:</legend>
         <div>
-            <input type="radio" id="facile" name="drone" value="facile" checked />
+            <input type="radio" id="facile" name="drone" value="easy" checked />
             <label for="facile">Facile</label>
         </div>
         <div>
-            <input type="radio" id="moyen" name="drone" value="moyen" />
+            <input type="radio" id="moyen" name="drone" value="medium" />
             <label for="dewey">Moyen</label>
         </div>
         <div>
-            <input type="radio" id="difficile" name="drone" value="difficile" />
+            <input type="radio" id="difficile" name="drone" value="hard" />
             <label for="louie">Difficile</label>
         </div>
     </fieldset>
