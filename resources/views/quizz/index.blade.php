babab7d717342c0ddb1978acfb1247ae5e9f0eb7
Lucy DO NASCIMENTO <lucynasci@gmail.com>
2023-08-30 14:02:32 +0200

    actualisation du form

--- resources/views/quizz/index.blade.php
+++ resources/views/quizz/index.blade.php
@@ -1,42 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quizz</title>
-</head>
-<body>
-<h1><b>Quizz</b></span></h1>
-<table border="1">
-    <tbody>
-        @foreach($quizz as $quizz)
-            <tr>
-                <td>{{ $quizz->id }}</td>
-                <td>{{ $quizz->pseudo }}</td>
-                <td>{{ $quizz->sujet }}</td>
-                <td>{{ $quizz->sujet }}</td>
-                <td>{{ $quizz->difficulte }}</td>
-                <td>
-                    <form action="">
-                        <label for="pseudo">Pseudonyme:</label><br>
-                        <input type="text" id="pseudo" name="pseudo"><br>
-                        
-                    </form>
-              </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-@if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
-
-@if (session('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
-    </div>
-@endif
-</body>
-</html>
+@section('formulaire')
+<form action="">
+    <label for="pseudo">Pseudonyme:</label><br>
+    <input type="text" id="pseudo" name="pseudo"><br>
+    <label for="pseudo">Sujet:</label><br>
+    <input type="text" id="sujet" name="sujet"><br>
+    <label for="pseudo">Difficulté.:</label><br>
+    <input type="text" id="difficulte" name="difficulte"><br>
+    <button type="button">Démarrer le Quizz</button>                    
+</form>
+ @endsection                   
