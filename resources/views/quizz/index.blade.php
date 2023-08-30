3a3a259a659d230f4e025316346cf4a65ae4087b
Lucy DO NASCIMENTO <lucynasci@gmail.com>
2023-08-30 15:04:18 +0200

    actualisation du form "sujet" et "difficulte"

--- resources/views/quizz/index.blade.php
+++ resources/views/quizz/index.blade.php
@@ -3,9 +3,24 @@
 <form action="">
     <label for="pseudo">Pseudonyme:</label><br>
     <input type="text" id="pseudo" name="pseudo"><br>
-    <label for="pseudo">Sujet:</label><br>
-    <input type="text" id="sujet" name="sujet"><br>
-    <label for="pseudo">Difficulté.:</label><br>
-    <input type="text" id="difficulte" name="difficulte"><br>
+    <select name="sujet" id="sujet">
+        <option value="">Sélectionner le sujet:</option>
+    </select>
+    <fieldset>
+        <legend>Sélectionner la difficulté:</legend>
+        <div>
+            <input type="radio" id="facile" name="drone" value="facile" checked />
+            <label for="facile">Facile</label>
+        </div>
+        <div>
+            <input type="radio" id="moyen" name="drone" value="moyen" />
+            <label for="dewey">Moyen</label>
+        </div>
+        <div>
+            <input type="radio" id="difficile" name="drone" value="difficile" />
+            <label for="louie">Difficile</label>
+        </div>
+    </fieldset>
     <button type="button">Démarrer le Quizz</button>                    
 </form>
+@endsection
