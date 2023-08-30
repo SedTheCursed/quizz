7f21734a6734bbd9bbf557da456a1a395179cb04
SedTheCursed <sed.robert@gmail.com>
2023-08-30 15:29:16 +0200

    récuperation des sujets du quizz

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Quizz;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(Quizz::class)->group(function(){
+    Route::get("/", "index");
 });
